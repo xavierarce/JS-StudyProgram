093614bfcdcb0347cdcffd1cd23240ec45db1ab8
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-06-10 17:37:58 -0500

    main

--- HTTP/JSON/AJAX + Asynchronous Javascript/promises.js
+++ HTTP/JSON/AJAX + Asynchronous Javascript/promises.js
@@ -1,14 +1,39 @@
-let p =  new Promise((resolve,reject)=>{
-    let a = 1+1
-    if (a==2){
-        resolve('Succes ')
-    }else {
-        reject('Failed')
-    }
-});
+// let p =  new Promise((resolve,reject)=>{
+//     let a = 1+2
+//     if (a==2){
+//         resolve('Succes')
+//     }else {
+//         reject('Failed')
+//     }
+// });
 
-p.then((message)=>{
-    console.log('This is in the then '+ message)
-}).catch((message)=>{
-    console.log('This is in the catch '+message)
+// p.then((message)=>{
+//     console.log('This is in the then '+ message)
+// }).catch((message)=>{
+//     console.log('This is in the catch '+message)
+// })
+
+const userLeft = false
+const userWatchingLoL = false
+
+function  learnCallback(callback, errorCallback){
+    if(userLeft){
+        errorCallback({
+            name: 'User Left',
+            message: ':('
+        })
+    } else if(userWatchingLoL){
+        errorCallback({
+            name:'User Warching LoL',
+            message:'JS < LOL'
+        })
+    } else{
+        callback('Learn!')
+    }
+};
+
+learnCallback((message)=>{
+    console.log('Success:'+ message)
+}, (error)=>{
+    console.log(error.name + ' ' + error.message)
 })

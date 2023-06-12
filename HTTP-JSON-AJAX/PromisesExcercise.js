0a73566c210973d99b5d15a76d81f2850286363d
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-06-12 18:34:10 -0500

    main

--- HTTP-JSON-AJAX/PromisesExcercise.js
+++ HTTP-JSON-AJAX/PromisesExcercise.js
@@ -2,27 +2,38 @@
 
 // #1) Create a promise that resolves in 4 seconds and returns "success" string
 const promise = new Promise((resolve,reject)=>{
-    setTimeout(resolve, 3000, 'Success')
-  })
+  setTimeout(resolve, 4000, 'Success')
+})
+
+const promise2 = new Promise((resolve,reject)=>{
+  setTimeout(() => {
+    resolve("Success2");
+  }, 4000)
+ })
   
-  // #2) Run the above promise and make it console.log "success"
-  promise.then(result=> console.log(result))
-  
-  // #3) Read about Promise.resolve() and Promise.reject(). How can you make
-  // the above promise shorter with Promise.resolve() and console loggin "success"
-  
-  
-  // #4) Catch this error and console log 'Ooops something went wrong'
-  //Promise.reject('failed')
-  
-  // #5) Use Promise.all to fetch all of these people from Star Wars (SWAPI) at the same time.
-  // Console.log the output and make sure it has a catch block as well.
-  const urls = [
-    'http://swapi.dev/api/people/1',
-    'http://swapi.dev/api/people/2',
-    'http://swapi.dev/api/people/3',
-    'http://swapi.dev/api/people/4'
-  ]
-  
-  // #6) Change one of your urls above to make it incorrect and fail the promise
-  // does your catch block handle it?
+// #2) Run the above promise and make it console.log "success"
+promise.then(console.log)
+promise2.then(result=> console.log(result))
+
+// #3) Read about Promise.resolve() and Promise.reject(). How can you make
+// the above promise shorter with Promise.resolve() and console loggin "success"
+const shortPromise = Promise.resolve(
+  setTimeout(() => {
+    console.log("ShortSuccess");
+  }, 4000)
+);
+// #4) Catch this error and console log 'Ooops something went wrong'
+Promise.reject('failed')
+  .catch(console.log('Ooops something went wrong'))
+
+// #5) Use Promise.all to fetch all of these people from Star Wars (SWAPI) at the same time.
+// Console.log the output and make sure it has a catch block as well.
+const urls = [
+  'http://swapi.dev/api/people/1',
+  'http://swapi.dev/api/people/2',
+  'http://swapi.dev/api/people/3',
+  'http://swapi.dev/api/people/4'
+]
+
+// #6) Change one of your urls above to make it incorrect and fail the promise
+// does your catch block handle it?

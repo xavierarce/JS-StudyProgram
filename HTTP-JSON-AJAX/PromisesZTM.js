6ea7fc4ac75583308442b6790662eb58ee7f5c0d
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-06-12 12:43:32 -0500

    main

--- HTTP-JSON-AJAX/PromisesZTM.js
+++ HTTP-JSON-AJAX/PromisesZTM.js
@@ -26,3 +26,19 @@ promise
 .then(result2=> result2 + '?')
 .catch(()=>console.log('Eroooor'))
 .then(result3=> console.log(result3 + ('?')))
+
+//////////////////////////////////////////////////////////////////////////////
+
+const urls = [
+'https://jsonplaceholder.typicode.com/users',
+'https://jsonplaceholder.typicode.com/posts',
+'https://jsonplaceholder.typicode.com/albums'
+]
+
+Promise.all(urls.map(url=>{
+return fetch(url).then(resp=> resp.json())
+})).then(results=>{
+console.log(results[0]),
+console.log(results[1]),
+console.log(results[2])
+}).catch(()=> console.log('EROOR'))

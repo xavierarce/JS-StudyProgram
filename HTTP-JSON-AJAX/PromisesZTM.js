b3d03c4666841e7d8406cdd4879dac629a5e17c9
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-06-12 12:24:38 -0500

    main

--- HTTP-JSON-AJAX/PromisesZTM.js
+++ HTTP-JSON-AJAX/PromisesZTM.js
@@ -6,6 +6,23 @@ const promise = new Promise((resolve,reject)=>{
     }
 })
 
+const promise2 = new Promise((resolve,reject)=>{
+setTimeout(resolve,100,'HIII')
+})
+
+const promise3 = new Promise((resolve,rejevt)=>{
+setTimeout(resolve,1000,'Pokieee')
+})
+
+const promise4 = new Promise((resolve,rejevt)=>{
+setTimeout(resolve,5000,'Its meee')
+})
+
+Promise.all([promise,promise2,promise3,promise4])
+.then(values => console.log(values))
+
 promise
-    .then(result=> result + '!')
-    .then(result=> console.log(result))
+.then(result=> result + '!')
+.then(result2=> result2 + '?')
+.catch(()=>console.log('Eroooor'))
+.then(result3=> console.log(result3 + ('?')))

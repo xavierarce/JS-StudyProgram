c6197e0ec4e99c4401b6a769ba1a5619cbbfae40
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-06-16 22:55:48 -0500

    main

---
+++ HTTP-JSON-AJAX/ES2020.js
@@ -0,0 +1,10 @@
+const promiseOne = new Promise((resolve,reject)=>
+setTimeout(resolve,3000))
+const promiseTwo = new Promise((resolve,reject)=>
+setTimeout(reject,6000))
+
+Promise.all([promiseOne,promiseTwo]).then(data=>console.log(data))
+.catch(e=>console.log('Something Failed', e))
+
+Promise.allSettled([promiseOne,promiseTwo]).then(data=>console.log(data))
+.catch(e=>console.log('Something Failed', e))
